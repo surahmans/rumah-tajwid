ce2b6d3cd3403f381e85e2916a589e65ad37e249
Surahman <surahman.duang@gmail.com>
2015-03-19 17:57:47 +0700

    Change images slideshow

--- resources/views/front/partials/slide.blade.php
+++ resources/views/front/partials/slide.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-xs-12">
         <div class="slide slide--active">
-            <img src="{{ asset('images/slide/1.jpeg') }}" class="img-rounded img-responsive" alt="Slide image"/>
+            <img src="{{ asset('images/slide/1.jpg') }}" class="img-rounded img-responsive" alt="Slide image"/>
             <div class="overlay">
                 <h3><a href="#">Pertama</a></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores consectetur dolorem ea eius ipsa magni, modi odit perspiciatis placeat quam rem repellendus rerum sed, similique. Aliquam labore laborum neque?</p>
@@ -9,7 +9,7 @@
         </div>
 
         <div class="slide">
-            <img src="{{ asset('images/slide/2.jpeg') }}" class="img-rounded img-responsive" alt="Slide image"/>
+            <img src="{{ asset('images/slide/2.jpg') }}" class="img-rounded img-responsive" alt="Slide image"/>
             <div class="overlay">
              <h3><a href="#">Kedua</a></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores consectetur dolorem ea eius ipsa magni, modi odit perspiciatis placeat quam rem repellendus rerum sed, similique. Aliquam labore laborum neque?</p>
@@ -17,7 +17,7 @@
         </div>
 
         <div class="slide">
-            <img src="{{ asset('images/slide/3.jpeg') }}" class="img-rounded img-responsive" alt="Slide image"/>
+            <img src="{{ asset('images/slide/3.jpg') }}" class="img-rounded img-responsive" alt="Slide image"/>
             <div class="overlay">
              <h3><a href="#">Ketiga</a></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores consectetur dolorem ea eius ipsa magni, modi odit perspiciatis placeat quam rem repellendus rerum sed, similique. Aliquam labore laborum neque?</p>
@@ -25,7 +25,7 @@
         </div>
 
         <div class="slide">
-            <img src="{{ asset('images/slide/4.jpeg') }}" class="img-rounded img-responsive" alt="Slide image"/>
+            <img src="{{ asset('images/slide/4.jpg') }}" class="img-rounded img-responsive" alt="Slide image"/>
             <div class="overlay">
              <h3><a href="#">Keempat</a></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores consectetur dolorem ea eius ipsa magni, modi odit perspiciatis placeat quam rem repellendus rerum sed, similique. Aliquam labore laborum neque?</p>
