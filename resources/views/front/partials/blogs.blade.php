3e6c76e8edb8f657e0f59bb3a0f5f1f26cc01bcd
Surahman <surahman.duang@gmail.com>
2015-03-20 13:25:29 +0700

    Add image, links, and button in blog's panel

--- resources/views/front/partials/blogs.blade.php
+++ resources/views/front/partials/blogs.blade.php
@@ -1,44 +1,100 @@
 <div class="row">
-    <div class="col-xs-12 col-md-6">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Artikel</h3>
-            </div>
-            <div class="panel-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, totam, voluptates. Animi, atque beatae cum dicta fugit hic id itaque magnam numquam odio optio porro quo tempora temporibus unde vitae!</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-xs-12 col-md-6">
+    <div class="col-xs-12 col-sm-6">
         <div class="panel panel-primary">
             <div class="panel-heading">
                 <h3 class="panel-title">Info Kegiatan</h3>
             </div>
             <div class="panel-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, totam, voluptates. Animi, atque beatae cum dicta fugit hic id itaque magnam numquam odio optio porro quo tempora temporibus unde vitae!</p>
+                <div class="blog">
+                    <div class="blog--latest">
+                        <img src="{{ asset('images/slide/1.jpg') }}" class="blog--latest--img img-rounded" alt=""/>
+                        <h3 class="blog--latest--img__overlay img-rounded">
+                            <a href="#">Kenali kesalahan umum dalam membaca al-Qur'an</a>
+                        </h3>
+                    </div>
+                    <div class="blog--links">
+                        <a href="#">Another link article</a>
+                        <a href="#">Again, Another link article</a>
+                        <a href="#">Lorem ipsum dolor sit</a>
+                        <a href="#">Pentingnya membaca dengan tajwid</a>
+                        <a href="#" class="more">More Articles</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="col-xs-12 col-md-6">
+    <div class="col-xs-12 col-sm-6">
         <div class="panel panel-primary">
             <div class="panel-heading">
-                <h3 class="panel-title">Berita</h3>
+                <h3 class="panel-title">Artikel</h3>
             </div>
             <div class="panel-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, totam, voluptates. Animi, atque beatae cum dicta fugit hic id itaque magnam numquam odio optio porro quo tempora temporibus unde vitae!</p>
+                <div class="blog">
+                    <div class="blog--latest">
+                        <img src="{{ asset('images/slide/2.jpg') }}" class="blog--latest--img img-rounded" alt=""/>
+                        <h3 class="blog--latest--img__overlay img-rounded">
+                            <a href="#">Kenali kesalahan umum dalam membaca al-Qur'an</a>
+                        </h3>
+                    </div>
+                    <div class="blog--links">
+                        <a href="#">Another link article</a>
+                        <a href="#">Again, Another link article</a>
+                        <a href="#">Lorem ipsum dolor sit</a>
+                        <a href="#">Pentingnya membaca dengan tajwid</a>
+                        <a href="#" class="more">More Articles</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="col-xs-12 col-md-6">
+    <div class="col-xs-12 col-sm-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">Info Pendaftaran</h3>
+            </div>
+            <div class="panel-body">
+                <div class="blog">
+                    <div class="blog--latest">
+                        <img src="{{ asset('images/slide/3.jpg') }}" class="blog--latest--img img-rounded" alt=""/>
+                        <h3 class="blog--latest--img__overlay img-rounded">
+                            <a href="#">Kenali kesalahan umum dalam membaca al-Qur'an</a>
+                        </h3>
+                    </div>
+                    <div class="blog--links">
+                        <a href="#">Another link article</a>
+                        <a href="#">Again, Another link article</a>
+                        <a href="#">Lorem ipsum dolor sit</a>
+                        <a href="#">Pentingnya membaca dengan tajwid</a>
+                        <a href="#" class="more">More Articles</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-6">
         <div class="panel panel-primary">
             <div class="panel-heading">
                 <h3 class="panel-title">Rumta Eropa</h3>
             </div>
             <div class="panel-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, totam, voluptates. Animi, atque beatae cum dicta fugit hic id itaque magnam numquam odio optio porro quo tempora temporibus unde vitae!</p>
+                <div class="blog">
+                    <div class="blog--latest">
+                        <img src="{{ asset('images/slide/4.jpg') }}" class="blog--latest--img img-rounded" alt=""/>
+                        <h3 class="blog--latest--img__overlay img-rounded">
+                            <a href="#">Kenali kesalahan umum dalam membaca al-Qur'an</a>
+                        </h3>
+                    </div>
+                    <div class="blog--links">
+                        <a href="#">Another link article</a>
+                        <a href="#">Again, Another link article</a>
+                        <a href="#">Lorem ipsum dolor sit</a>
+                        <a href="#">Pentingnya membaca dengan tajwid</a>
+                        <a href="#" class="more">More Articles</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
