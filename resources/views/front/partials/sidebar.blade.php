48442b6d13e620acbc441bd1bf70137b13abda50
Surahman <surahman.duang@gmail.com>
2015-03-21 08:15:35 +0700

    Add style to sidebar

--- resources/views/front/partials/sidebar.blade.php
+++ resources/views/front/partials/sidebar.blade.php
@@ -1,19 +1,20 @@
 <div class="row">
     <div class="col-xs-12">
-        <h3>Jadwal Sholat</h3>
         <div class="col-xs-12">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam blanditiis dolor et quaerat? Ab assumenda blanditiis commodi cumque error, exercitationem illum, incidunt inventore iste, nisi quae quasi totam vel!</p>
+            <div class="panel panel-info">
+            	  <div class="panel-heading">
+            			<h3 class="panel-title text-center"><strong>Jadwal Sholat</strong></h3>
+            	  </div>
+            	  <div class="panel-body">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam blanditiis dolor et quaerat? Ab assumenda blanditiis commodi cumque error, exercitationem illum, incidunt inventore iste, nisi quae quasi totam vel!</p>
+            	  </div>
+            </div>
         </div>
     </div>
+
     <div class="col-xs-12">
-        <h3>Artikel Newsfeed</h3>
         <div class="col-xs-12">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam blanditiis dolor et quaerat? Ab assumenda blanditiis commodi cumque error, exercitationem illum, incidunt inventore iste, nisi quae quasi totam vel!</p>
-        </div>
-    </div>
-    <div class="col-xs-12">
-        <h3>Advertise</h3>
-        <div class="col-xs-12">
+            <h3>Advertise</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam blanditiis dolor et quaerat? Ab assumenda blanditiis commodi cumque error, exercitationem illum, incidunt inventore iste, nisi quae quasi totam vel!</p>
         </div>
     </div>
