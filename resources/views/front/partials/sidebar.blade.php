ca1af2e494606f123b68140074d67284c5a34673
Surahman <surahman.duang@gmail.com>
2015-03-22 15:14:22 +0700

    Add tab pane layout on sidebar

--- resources/views/front/partials/sidebar.blade.php
+++ resources/views/front/partials/sidebar.blade.php
@@ -14,8 +14,22 @@
 
     <div class="col-xs-12">
         <div class="col-xs-12">
-            <h3>Advertise</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam blanditiis dolor et quaerat? Ab assumenda blanditiis commodi cumque error, exercitationem illum, incidunt inventore iste, nisi quae quasi totam vel!</p>
+            <div class="panel with-nav-tabs panel-info">
+                <div class="panel-heading">
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#popular" data-toggle="tab">Populer</a></li>
+                        <li><a href="#recent" data-toggle="tab">Terkini</a></li>
+                        <li><a href="#comment" data-toggle="tab">Komentar</a></li>
+                    </ul>
+                </div>
+                <div class="panel-body">
+                    <div class="tab-content">
+                        <div class="tab-pane fade in active" id="popular">Default 1</div>
+                        <div class="tab-pane fade" id="recent">Default 2</div>
+                        <div class="tab-pane fade" id="comment">Default 3</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
