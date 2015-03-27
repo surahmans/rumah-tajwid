4a3973b7cf8161abb9a2682e8806392b62b4f5c2
Surahman <surahman.duang@gmail.com>
2015-03-27 22:38:10 +0700

    Blog posts by category

--- resources/views/front/partials/blogs.blade.php
+++ resources/views/front/partials/blogs.blade.php
@@ -1,101 +1,59 @@
 <div class="row">
     <div class="col-xs-12 col-sm-6">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Info Kegiatan</h3>
-            </div>
-            <div class="panel-body">
-                <div class="blog">
-                    <div class="blog--latest">
-                        <img src="{{ asset('images/slide/1.jpg') }}" class="blog--latest--img img-rounded" alt=""/>
-                        <h3 class="blog--latest--img__overlay img-rounded">
-                            <a href="#">Kenali kesalahan umum dalam membaca al-Qur'an</a>
-                        </h3>
-                    </div>
-                    <div class="blog--links">
-                        <a href="#">Another link article</a>
-                        <a href="#">Again, Another link article</a>
-                        <a href="#">Lorem ipsum dolor sit</a>
-                        <a href="#">Pentingnya membaca dengan tajwid</a>
-                        <a href="#" class="more">More Articles</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        <div class="row">
+            <?php $col = 2; //set blog column ?>
+            @foreach($blogs as $blog)
 
-    <div class="col-xs-12 col-sm-6">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Artikel</h3>
-            </div>
-            <div class="panel-body">
-                <div class="blog">
-                    <div class="blog--latest">
-                        <img src="{{ asset('images/slide/2.jpg') }}" class="blog--latest--img img-rounded" alt=""/>
-                        <h3 class="blog--latest--img__overlay img-rounded">
-                            <a href="#">Kenali kesalahan umum dalam membaca al-Qur'an</a>
-                        </h3>
-                    </div>
-                    <div class="blog--links">
-                        <a href="#">Another link article</a>
-                        <a href="#">Again, Another link article</a>
-                        <a href="#">Lorem ipsum dolor sit</a>
-                        <a href="#">Pentingnya membaca dengan tajwid</a>
-                        <a href="#" class="more">More Articles</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                @if($col > 0)
+                        <?php $col--; //rest of column ?>
+                        <?php $limit = 5; $latest = 1; ?>
 
-    <div class="col-xs-12 col-sm-6">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Info Pendaftaran</h3>
-            </div>
-            <div class="panel-body">
-                <div class="blog">
-                    <div class="blog--latest">
-                        <img src="{{ asset('images/slide/3.jpg') }}" class="blog--latest--img img-rounded" alt=""/>
-                        <h3 class="blog--latest--img__overlay img-rounded">
-                            <a href="#">Kenali kesalahan umum dalam membaca al-Qur'an</a>
-                        </h3>
-                    </div>
-                    <div class="blog--links">
-                        <a href="#">Another link article</a>
-                        <a href="#">Again, Another link article</a>
-                        <a href="#">Lorem ipsum dolor sit</a>
-                        <a href="#">Pentingnya membaca dengan tajwid</a>
-                        <a href="#" class="more">More Articles</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                        @if($limit > count($blog->articles))
+                            <?php $limit = count($blog->articles); ?>
+                        @endif
 
-    <div class="col-xs-12 col-sm-6">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Rumta Eropa</h3>
-            </div>
-            <div class="panel-body">
-                <div class="blog">
-                    <div class="blog--latest">
-                        <img src="{{ asset('images/slide/4.jpg') }}" class="blog--latest--img img-rounded" alt=""/>
-                        <h3 class="blog--latest--img__overlay img-rounded">
-                            <a href="#">Kenali kesalahan umum dalam membaca al-Qur'an</a>
-                        </h3>
-                    </div>
-                    <div class="blog--links">
-                        <a href="#">Another link article</a>
-                        <a href="#">Again, Another link article</a>
-                        <a href="#">Lorem ipsum dolor sit</a>
-                        <a href="#">Pentingnya membaca dengan tajwid</a>
-                        <a href="#" class="more">More Articles</a>
-                    </div>
-                </div>
-            </div>
+                        @foreach($blog->articles as $article)
+                            @if($latest > 0)
+                                <div class="col-xs-12">
+                                    <div class="panel panel-primary">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title">{{ $blog->name }}</h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <div class="blog">
+                                                <div class="blog--latest">
+                                                    <img src="{{ 'images/slide/' . $article->cover }}" class="blog--latest--img img-rounded" alt=""/>
+                                                    <h3 class="blog--latest--img__overlay img-rounded">
+                                                        <a href="{{ $article->slug }}">{{ $article->title }}</a>
+                                                    </h3>
+                                                </div>
+                                                <div class="blog--links">
+                            @else
+                                                    <a href="{{ $article->slug }}">{{ $article->title }}</a>
+                            @endif
+
+                            <?php $latest--; ?>
+                            <?php $limit--; ?>
+
+                            @if($limit <= 0 || count($blog->articles) == 0)
+                                                    <a href="{{ $blog->slug }}" class="more">More Articles</a>
+                                                </div> <!-- Blog links -->
+                                            </div> <!-- Blog -->
+                                        </div> <!-- Blog panel body -->
+                                    </div> <!-- End of blog panel -->
+                                </div> <!-- End of blog column -->
+                                <?php break; //Next category ?>
+                            @endif
+                        @endforeach
+                    @if($col == 0)
+                        <?php $col = 2; //set blog column again ?>
+                                </div> <!-- end of column -->
+                            </div> <!-- end of blog row -->
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="row">
+                    @endif
+                @endif
+            @endforeach
         </div>
-    </div>
-</div>
+    </div> <!-- End of Blogs column -->
+</div> <!-- End of Blogs row -->
