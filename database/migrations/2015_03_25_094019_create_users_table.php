d1bbfc2e5ba5afe6d7bbf616ab225550023dccb5
Surahman <surahman.duang@gmail.com>
2015-04-09 18:57:39 +0700

    Change default level to author

--- database/migrations/2015_03_25_094019_create_users_table.php
+++ database/migrations/2015_03_25_094019_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration {
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('level');
+            $table->string('level')->default('author');
             $table->rememberToken();
             $table->timestamps();
         });
