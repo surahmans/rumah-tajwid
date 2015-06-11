291ffe59c1676fb869f8ddf2dbc22af02fce2165
Surahman <surahman.duang@gmail.com>
2015-06-11 22:14:53 +0700

    fix articles position

--- resources/views/front/partials/articles.blade.php
+++ resources/views/front/partials/articles.blade.php
@@ -1,18 +1,20 @@
-<div class="row">
-    @foreach($articles as $article)
-        <div class="col-xs-12 col-sm-6">
-            <div class="panel panel-primary">
-                <div class="panel-body">
-                    <img src="{{ asset('images/blog/' . $article->cover) }}" class="img-intro img-rounded" alt=""/>
-                    <h3 class="h4">{{ $article->title }}</h3>
-                    <p>{{ $article->excerpt }}</p>
-                    <p>
-                        <a href="{{ action('ArticleController@show', [$article->slug]) }}" class="readmore pull-right">{{ $configs->readmore }}</a>
-                    </p>
+@foreach($articles->chunk(2) as $row)
+    <div class="row">
+        @foreach($row as $article)        
+            <div class="col-xs-12 col-sm-6">
+                <div class="panel panel-primary">
+                    <div class="panel-body">
+                        <img src="{{ asset('images/blog/' . $article->cover) }}" class="img-intro img-rounded" alt=""/>
+                        <h3 class="h4">{{ $article->title }}</h3>
+                        <p>{{ $article->excerpt }}</p>
+                        <p>
+                            <a href="{{ action('ArticleController@show', [$article->slug]) }}" class="readmore pull-right">{{ $configs->readmore }}</a>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    @endforeach
-</div>
+        @endforeach
+    </div>
+@endforeach
 
 @include('front.partials.pagination', $articles)
